5a91da54940e4a098cd020809f34133698e6b6bf
FDVarelo <fdvarelo@gmail.com>
2022-06-28 15:47:45 -0300

    first commit

---
+++ Course/Course/Entities/AccountBusiness.cs
@@ -0,0 +1,28 @@
+﻿namespace Course.Entities
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+
+        }
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance) // Aproveitamento
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Load(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+            else
+            {
+                Console.WriteLine("\n> !You exceded the amount o load that can be used!");
+            }
+        }
+    }
+}
---
+++ Course/Course/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount accountBusiness = new BusinessAccount(8822, "Alex Gomes", 500.00, 1000.00);
+            Account accountUser = new Account(8846, "Fred Varelo", 1200.00);
+
+            Console.WriteLine($"> Account: {accountUser.Number} from {accountUser.Holder} has ${accountUser.Balance} in the accountBusiness.\n");
+            Console.WriteLine($"> The Business Account: {accountBusiness.Number} from {accountBusiness.Holder} has ${accountBusiness.Balance} in the account, and can loan the maximun ammount of ${accountBusiness.LoanLimit}.\n");
+            Console.Write($"> Ammount of loan for the  Business Account {accountBusiness.Number}: ");
+            double amount = double.Parse(Console.ReadLine());
+            accountBusiness.Load(amount);
+            Console.WriteLine($"\n> Updated Balance for Business Account {accountBusiness.Number}: ${accountBusiness.Balance}");
+        }
+    }
+}
