6aad536b9b1c4c58491955f0ee63e09a096d0bb7
Fred Daniel Varelo <fdvarelo@gmail.com>
2022-06-30 09:31:30 -0300

    Exercício Proposto

---
+++ Course/Course3/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Course3.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course3.Entities
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee()
+        {
+
+        }
+        public OutsourcedEmployee(string name, int hours, double valuePerHour,double additionalCharge) 
+            : base(name,hours,valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            double var = base.Payment(); // var recebe (horas trabalhadas * valor por hora)
+            return var += AdditionalCharge + AdditionalCharge*0.1; // var recebe (var + valor adicional + 10% do valor adicional)
+
+            // return base.Payment() + 1.1 * AdditionalCharge; // Outra forma de fazer os calculos acima, porém menos legivel
+        }
+    }
+}
---
+++ Course/Course3/Program.cs
@@ -0,0 +1,65 @@
+﻿using Course3.Entities;
+using System;
+using System.Globalization;
+
+namespace Course3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> Employees = new List<Employee>();
+            Console.Write("Enter the number of employees: "); // Quantidade de funcionarios a serem pegos os dados
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"\nEmployee {i+1} data:");
+                Console.Write("Outsourced (y/n): "); // É un funcionario da empresa, ou terceirizado?
+                char verifyEmployee = char.Parse(Console.ReadLine());
+
+                
+
+                if (verifyEmployee == 'n')
+                {
+                    // Inicio dos dados do funcionario
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Hours: ");
+                    int hours = int.Parse(Console.ReadLine());
+                    Console.Write("Value per hour: ");
+                    double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    Employees.Add(new Employee(name, hours, valuePerHour));
+                }
+                else if(verifyEmployee == 'y')
+                {
+                    // Inicio dos dados do funcionario terceirizado
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Hours: ");
+                    int hours = int.Parse(Console.ReadLine());
+                    Console.Write("Value per hour: ");
+                    double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Additional charge: ");
+                    double addCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Employees.Add(new OutsourcedEmployee(name, hours, valuePerHour, addCharge));
+                }
+                else
+                {
+                    Console.WriteLine("\n!The option of outsourced is only (y/n), try again!\n");
+                    i--;
+                }
+
+            }
+
+            // Printar todos os funcionarios e os valores a serem pagos a eles
+            Console.WriteLine("\nPayments:");
+
+            foreach(Employee index in Employees)
+            {
+                Console.WriteLine(index.Name + " - $ " + index.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
